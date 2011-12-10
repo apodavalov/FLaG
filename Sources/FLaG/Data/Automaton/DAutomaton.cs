2e266d5c1b21f85c035f79648cad0f5df56bb963
Anton Podavalov <a.podavalov@gmail.com>
2011-12-10 18:42:44 +0400

    Fully implemented removing unreached symbols

--- Sources/FLaG/Data/Automaton/DAutomaton.cs
+++ Sources/FLaG/Data/Automaton/DAutomaton.cs
@@ -5,10 +5,232 @@ using FLaG.Output;
 namespace FLaG.Data.Automaton
 {
 	class DAutomaton
-	{
-		public void RemoveUnreachedStates (Writer par1)
+	{		
+		public void SaveP(Writer writer, int number)
 		{
-			throw new NotImplementedException ();
+			writer.Write(@"{");
+			writer.Write(@"P");
+			writer.Write("_{");
+			writer.Write(number);
+			writer.Write(@"}");
+			writer.Write(@"}");
+		}
+		
+		public void SaveR (Writer writer)
+		{
+			writer.WriteLine("R");
+		}
+		
+		public void RemoveUnreachedStates (Writer writer)
+		{
+			writer.WriteLine(@"Выполним удаление недостижимых состояний построенного ДКА. Для ",true);
+			writer.WriteLine(@"этого будем использовать два дополнительных множества: ",true);
+			writer.WriteLine(@"множество достижим состояний ",true);
+			writer.WriteLine(@"\begin{math}");
+			SaveR(writer);
+			writer.WriteLine(@"\end{math}");
+			writer.WriteLine(@"и множество текущих активных состояний на каждом шаге алгоритма ",true);
+			writer.WriteLine(@"\begin{math}");
+			writer.WriteLine("P_i");
+			writer.WriteLine(@"\end{math}.");
+			writer.WriteLine(@"Результатом работы алгоритма является полное множество достижимых состояний.",true);
+			writer.WriteLine();
+			writer.WriteLine(@"На первом шаге работы алгоритма получаем",true);
+			
+			int i = 0;
+			List<DStatus> R = new List<DStatus>();
+			List<DStatus> P = new List<DStatus>();
+			
+			AddStatus(R,InitialStatus);
+			AddStatus(P,InitialStatus);
+			
+			writer.WriteLine(@"\begin{math}");
+			SaveR(writer);
+			writer.WriteLine(@"=");
+			SaveStatuses(writer, R.ToArray(),false);
+			writer.WriteLine(@"\end{math},");
+			writer.WriteLine(@"\begin{math}");
+			writer.WriteLine(@"i=");
+			writer.WriteLine(i);
+			writer.WriteLine(@"\end{math},");
+			writer.WriteLine(@"\begin{math}");
+			SaveP(writer,i);
+			writer.WriteLine(@"=");
+			SaveStatuses(writer,P.ToArray(),false);
+			writer.WriteLine(@"\end{math}.");
+			
+			i++;
+			
+			writer.WriteLine();
+			writer.WriteLine(@"Далее множество ",true);
+			writer.WriteLine(@"\begin{math}");
+			SaveP(writer,i);
+			writer.WriteLine(@"\end{math}");	
+			writer.WriteLine("примет следующий вид",true);
+			
+			bool somethingChanged;
+			
+			do
+			{
+				somethingChanged = false;
+				
+				List<DStatus> newP = new List<DStatus>();
+				
+				foreach (DTransitionFunc func in Functions)
+					if (P.BinarySearch(func.OldStatus) >= 0)
+						AddStatus(newP,func.NewStatus);
+
+				writer.WriteLine();
+				writer.WriteLine(@"\begin{math}");
+				SaveP(writer,i);
+				writer.WriteLine(@"=");
+				SaveStatuses(writer,newP.ToArray(),false);
+				writer.WriteLine(@"\end{math}.");
+				writer.WriteLine();
+				writer.WriteLine(@"Рассматриваем разность двух множеств",true);
+				writer.WriteLine(@"\begin{math}");
+				SaveP(writer,i);
+				writer.WriteLine(@"\end{math}");
+				writer.WriteLine(@"и",true);
+				writer.WriteLine(@"\begin{math}");
+				SaveR(writer);
+				writer.WriteLine(@"\end{math}.");
+				writer.WriteLine();
+				writer.WriteLine(@"\begin{math}");
+				SaveP(writer,i);
+				writer.WriteLine(@"\setminus");
+				SaveR(writer);
+				writer.WriteLine(@"=");
+				SaveStatuses(writer,newP.ToArray(),false);
+				writer.WriteLine(@"\setminus");
+				SaveStatuses(writer,R.ToArray(),false);
+				writer.WriteLine(@"=");
+				
+				foreach (DStatus status in R)
+				{
+					int index = newP.BinarySearch(status);
+					
+					if (index >= 0)
+						newP.RemoveAt(index);
+				}
+				SaveStatuses(writer,newP.ToArray(),false);
+				writer.WriteLine(@"\end{math}.");
+				
+				writer.WriteLine();
+				
+				if (newP.Count != 0)
+				{
+					writer.WriteLine(@"Так как разность этих двух множеств не пустое множество,", true);
+					writer.WriteLine(@"то к множеству ", true);
+					writer.WriteLine(@"\begin{math}");
+					SaveR(writer);
+					writer.WriteLine(@"\end{math}.");
+					writer.WriteLine(@"добавляем элементы из множества", true);
+					writer.WriteLine(@"\begin{math}");
+					SaveP(writer,i);
+					writer.WriteLine(@"\end{math}");
+					writer.WriteLine();
+					writer.WriteLine(@"\begin{math}");
+					SaveR(writer);
+					writer.WriteLine(@"\cup");
+					SaveP(writer,i);
+					writer.WriteLine(@"=");
+					SaveStatuses(writer,R.ToArray(),false);
+					writer.WriteLine(@"\cup");
+					SaveStatuses(writer,newP.ToArray(),false);
+					
+					foreach (DStatus status in newP)
+						AddStatus(R,status);
+					
+					writer.WriteLine(@"=");
+					SaveStatuses(writer,R.ToArray(),false);
+					
+					writer.WriteLine(@"\end{math}");
+					writer.WriteLine();
+					writer.WriteLine(@"Алгоритм продолжаем дальше.");
+					i++;
+					somethingChanged = true;
+					P = newP;
+					
+					writer.WriteLine(@"На следующем шаге алгоритма строим множество",true);
+					writer.WriteLine(@"\begin{math}");
+					SaveP(writer,i);
+					writer.WriteLine(@"\end{math}.");
+					writer.WriteLine(@"В это множество включаются все состояния, в которые",true);
+					writer.WriteLine(@"можно перейти по функциям перехода из состояний,",true);
+					writer.WriteLine(@"входящих в множество",true);
+					writer.WriteLine(@"\begin{math}");
+					SaveP(writer,i-1);
+					writer.WriteLine(@"\end{math}.");
+					writer.WriteLine(@"Таким образом, множество текущих активных состояний примет следующий вид",true);					
+				}
+				else
+				{
+					writer.WriteLine(@"Так как разность этих двух множеств пустое множество,", true);
+					writer.WriteLine(@"то выполнение алгоритма недостижимых состояний заканчивается.", true);
+				}
+			} while (somethingChanged);
+			
+			writer.WriteLine();
+			writer.WriteLine(@"После выполнения этого алгоритма из множества всех состояний ДКА", true);
+			writer.WriteLine(@"удаляем недостижимые состояния. Таким образом, множество состояний ДКА примет вид", true);
+			
+			for (i = 0; i < Functions.Count; i++)
+			{
+				if (R.BinarySearch(Functions[i].OldStatus) < 0)
+				{
+					Functions.RemoveAt(i);
+					i--;
+				}
+			}
+			
+			DStatus[] oldEndStatuses = EndStatuses.ToArray();
+			EndStatuses.Clear();
+			DStatus[] currentStatuses = Statuses;
+			
+			foreach (DStatus status in oldEndStatuses)
+				if (Array.BinarySearch<DStatus>(currentStatuses,status) >= 0)
+					AddEndStatus(status);
+			
+			writer.WriteLine();
+			
+			writer.WriteLine(@"\begin{math}");
+			SaveQ(writer);
+			writer.WriteLine(@"=");
+			SaveStatuses(writer,false);			
+			writer.WriteLine(@"\end{math}");
+			writer.WriteLine();
+			writer.WriteLine(@"Множество функций переходов эквивалентного ДКА преобразуется к виду",true);
+			writer.WriteLine();
+			writer.WriteLine(@"\begin{math}");
+			SaveDelta(writer);
+			writer.WriteLine(@"=");
+			SaveFunctions(writer,false);
+			writer.WriteLine(@"\end{math}");
+			writer.WriteLine();
+			writer.WriteLine(@"Начальным состоянием эквивалентного ДКА",true);
+			writer.WriteLine(@"\begin{math}");
+			SaveM(writer);
+			writer.WriteLine(@"\end{math}");
+			writer.WriteLine(@"будет состояние",true);
+			writer.WriteLine();
+			writer.WriteLine(@"\begin{math}");
+			SaveQ0(writer);
+			writer.WriteLine(@"=");
+			InitialStatus.Save(writer,IsLeft,ProducedFromDFA);
+			writer.WriteLine(@"\end{math}.");
+			writer.WriteLine();
+			writer.WriteLine(@"Множество заключительных состояний ДКА",true);
+			writer.WriteLine(@"\begin{math}");
+			SaveM(writer);
+			writer.WriteLine(@"\end{math}");
+			writer.WriteLine(@"примет вид",true);
+			writer.WriteLine();
+			writer.WriteLine(@"\begin{math}");
+			SaveS(writer);
+			writer.WriteLine(@"=");
+			SaveEndStatuses(writer,false);
+			writer.WriteLine(@"\end{math}");
 		}
 		
 		public void SaveFunctionsShort (Writer writer)
