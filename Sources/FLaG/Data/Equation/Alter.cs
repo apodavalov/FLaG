5f264bb523c34d0692cae7ff1054271698311a90
Anton Podavalov <a.podavalov@gmail.com>
2011-12-18 19:19:20 +0400

    Implemented optimization for Alter

--- Sources/FLaG/Data/Equation/Alter.cs
+++ Sources/FLaG/Data/Equation/Alter.cs
@@ -48,8 +48,115 @@ namespace FLaG.Data.Equation
 
 		public override Expression Optimize ()
 		{
-			// TODO: оптимизировать
-			return this;
+			List<Expression> list = new List<Expression>();
+			
+			foreach (Expression e in Expressions)
+				list.Add(e);
+			
+			Expressions.Clear();
+			
+			for (int i = 0; i < list.Count; i++)
+				list[i] = list[i].Optimize();
+			
+			bool somethingChanged;
+			
+			do
+			{
+				somethingChanged = false;			
+			
+				// поднимаем альтернативы
+				for (int i = 0; i < list.Count; i++)
+					if (list[i] is Alter)
+					{
+						Alter a = (Alter)list[i];
+						list.RemoveAt(i);
+						foreach (Expression e in a.Expressions)
+							list.Insert(i++,e);
+						i--;						
+						somethingChanged = true;
+					}
+				
+				bool haveEmpty = false;
+				
+				for (int i = 0; i < list.Count; i++)
+					if (list[i] is Empty || list[i] is Repeat && !((Repeat)list[i]).AtLeastOne)
+					{
+						haveEmpty = true;
+						break;
+					}
+				
+				if (haveEmpty)
+					for (int i = 0; i < list.Count; i++)
+					{
+						if (list[i] is Repeat)		
+						{
+							Repeat repeat = (Repeat)list[i];
+							if (repeat.AtLeastOne)
+							{	
+								repeat.AtLeastOne = false;
+								list[i] = repeat.Optimize();
+								somethingChanged = true;
+							}
+						}
+					}
+				
+				bool haveRepeatWithAtLeastOneFalse = false;
+				
+				for (int i = 0; i < list.Count; i++)
+					if (list[i] is Repeat && !((Repeat)list[i]).AtLeastOne)
+					{
+						haveRepeatWithAtLeastOneFalse = true;
+						break;
+					}
+				
+				if (haveRepeatWithAtLeastOneFalse)
+					for (int i = 0; i < list.Count; i++)
+						if (list[i] is Empty)
+						{
+							list.RemoveAt(i);
+							i--;
+							somethingChanged = true;
+						}
+				
+				List<Repeat> repeats = new List<Repeat>();
+				
+				for (int i = 0; i < list.Count; i++)
+					if (list[i] is Repeat)
+						repeats.Add((Repeat)list[i]);
+				
+				for (int i = 0; i < repeats.Count; i++)
+					for (int j = 0; j < list.Count; j++)
+					{
+						bool rem = false;
+					
+						if (list[j].Equals(repeats[i].Expression))
+							rem = true;
+						else if (list[j] is Degree)
+						{
+							Degree d = (Degree)list[j];
+							if (d.Base.Equals(repeats[i].Expression))
+								rem = true;
+						}
+					
+						if (rem)
+						{
+							list.RemoveAt(j);
+							somethingChanged = true;
+							j--;
+						}
+					}
+				
+			} while (somethingChanged);
+			
+			foreach (Expression e in list)
+				Expressions.Add(e);
+
+			if (list.Count == 0)
+				return new Empty();
+			else if (list.Count == 1)
+				return list[0];
+			else
+				return this;				
 		}
 
 		public override void Save(Writer writer)
