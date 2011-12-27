3246c9b4cb7cae27bc0f225fc5e8ce017740f1c5
Anton Podavalov <a.podavalov@gmail.com>
2011-12-27 22:39:19 +0400

    Remove apostroph for Unterminal output and for DAutomaton

--- Sources/FLaG/Data/Grammars/Unterminal.cs
+++ Sources/FLaG/Data/Grammars/Unterminal.cs
@@ -37,16 +37,10 @@ namespace FLaG.Data.Grammars
 		public override void Save(Writer writer, bool isLeft)
 		{
 			writer.Write(@"{");
-			writer.Write(@"{");
 			writer.Write("S", true);
 			writer.Write("_{");
 			writer.Write(Number);
 			writer.Write(@"}}");
-			if (isLeft)
-				writer.Write("'");
-			else
-				writer.Write("''");
-			writer.Write(@"}");
 		}
 
 		public int CompareTo(Unterminal other)
