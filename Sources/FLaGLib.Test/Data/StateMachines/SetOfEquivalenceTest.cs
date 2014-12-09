2c3bcea61afa8534795f4f4e460c18425284bd5f
Anton Podavalov <a.podavalov@gmail.com>
2014-12-08 20:53:23 -0800

    Added SetOfEquivalence tests.

--- Sources/FLaGLib.Test/Data/StateMachines/SetOfEquivalenceTest.cs
+++ Sources/FLaGLib.Test/Data/StateMachines/SetOfEquivalenceTest.cs
@@ -1,15 +1,107 @@
-﻿using NUnit.Framework;
+﻿using FLaGLib.Data;
+using FLaGLib.Data.StateMachines;
+using FLaGLib.Helpers;
+using FLaGLib.Test.TestHelpers;
+using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 
 namespace FLaGLib.Test.Data.StateMachines
 {
     [TestFixture]
     public class SetOfEquivalenceTest
     {
-        [Test]
-        public void NoOneTestHasBeenImplemented()
+        private Tuple<SetOfEquivalence, SetOfEquivalence, int>[] _Expectations = new Tuple<SetOfEquivalence, SetOfEquivalence, int>[]
         {
-            throw new NotImplementedException();
+            new Tuple<SetOfEquivalence, SetOfEquivalence, int>(null,null,0),
+            new Tuple<SetOfEquivalence, SetOfEquivalence, int>(
+                new SetOfEquivalence(
+                    new SortedSet<Label>(
+                        EnumerateHelper.Sequence(
+                            new Label(new SingleLabel('A')),
+                            new Label(new SingleLabel('B'))
+                        )
+                    )),null,1),
+            new Tuple<SetOfEquivalence, SetOfEquivalence, int>(
+                new SetOfEquivalence(
+                    new SortedSet<Label>(
+                        EnumerateHelper.Sequence(
+                            new Label(new SingleLabel('A')),
+                            new Label(new SingleLabel('B'))
+                        )
+                    )),
+                new SetOfEquivalence(
+                    new SortedSet<Label>(
+                        EnumerateHelper.Sequence(
+                            new Label(new SingleLabel('A')),
+                            new Label(new SingleLabel('B'))
+                        )
+                    )),0),
+            new Tuple<SetOfEquivalence, SetOfEquivalence, int>(
+                new SetOfEquivalence(
+                    new SortedSet<Label>(
+                        EnumerateHelper.Sequence(
+                            new Label(new SingleLabel('C')),
+                            new Label(new SingleLabel('B'))
+                        )
+                    )),
+                new SetOfEquivalence(
+                    new SortedSet<Label>(
+                        EnumerateHelper.Sequence(
+                            new Label(new SingleLabel('A')),
+                            new Label(new SingleLabel('B'))
+                        )
+                    )),1),
+            new Tuple<SetOfEquivalence, SetOfEquivalence, int>(
+                new SetOfEquivalence(
+                    new SortedSet<Label>(
+                        EnumerateHelper.Sequence(
+                            new Label(new SingleLabel('C')),
+                            null,
+                            new Label(new SingleLabel('B'))
+                        )
+                    )),
+                new SetOfEquivalence(
+                    new SortedSet<Label>(
+                        EnumerateHelper.Sequence(
+                            new Label(new SingleLabel('A')),
+                            new Label(new SingleLabel('B'))
+                        )
+                    )),-1)
+        };
+
+        [Test]
+        public void CctorTest_Ok()
+        {
+            ISet<Label> expected = new SortedSet<Label>(
+                EnumerateHelper.Sequence(
+                    new Label(new SingleLabel('A')), 
+                    new Label(new SingleLabel('C'))
+                )
+            );
+
+            SetOfEquivalence actual = new SetOfEquivalence(expected);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CctorTest_SetNull_Fail()
+        {
+            new SetOfEquivalence(null);
+        }
+
+        [Test]
+        public void EqualsTest()
+        {
+            ComparableEquatableHelper.TestEquals(_Expectations);
+        }
+
+        [Test]
+        public void CompareTest()
+        {
+            ComparableEquatableHelper.TestCompare(_Expectations);
         }
     }
 }
