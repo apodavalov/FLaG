186039450c273da96299c0fd32c6a293a082c1d7
Lenuza Podavalova <l.podavalova@gmail.com>
2014-12-31 14:45:20 +0300

    Added tests for ClassOfEquivalenceSet.

--- Sources/FLaGLib.Test/Data/StateMachines/ClassOfEquivalenceSetTest.cs
+++ Sources/FLaGLib.Test/Data/StateMachines/ClassOfEquivalenceSetTest.cs
@@ -1,4 +1,8 @@
-﻿using NUnit.Framework;
+﻿using FLaGLib.Data.StateMachines;
+using FLaGLib.Helpers;
+using FLaGLib.Test.TestHelpers;
+using FLaGLib.Extensions;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +14,157 @@ namespace FLaGLib.Test.Data.StateMachines
     [TestFixture]
     public class ClassOfEquivalenceSetTest
     {
+        private Tuple<ClassOfEquivalenceSet, ClassOfEquivalenceSet, int>[] _Expectations = new Tuple<ClassOfEquivalenceSet, ClassOfEquivalenceSet, int>[]
+        {
+            new Tuple<ClassOfEquivalenceSet, ClassOfEquivalenceSet, int>(
+                null,
+                null,
+                0
+            ),
+            new Tuple<ClassOfEquivalenceSet, ClassOfEquivalenceSet, int>(
+                null,
+                new ClassOfEquivalenceSet(
+                    new SortedSet<ClassOfEquivalence>(
+                        EnumerateHelper.Sequence(
+                            new ClassOfEquivalence(
+                                1,
+                                new SortedSet<char>(
+                                    EnumerateHelper.Sequence('a', 'b')
+                                ).AsReadOnly()
+                            )
+                        )
+                    )
+                ),
+                -1
+            ),
+            new Tuple<ClassOfEquivalenceSet, ClassOfEquivalenceSet, int>(
+                new ClassOfEquivalenceSet(
+                    new SortedSet<ClassOfEquivalence>(
+                        EnumerateHelper.Sequence(
+                            new ClassOfEquivalence(
+                                1,
+                                new SortedSet<char>(
+                                    EnumerateHelper.Sequence('a', 'b')
+                                ).AsReadOnly()
+                            )
+                        )
+                    )
+                ),
+                null,
+                1
+            ),
+            new Tuple<ClassOfEquivalenceSet, ClassOfEquivalenceSet, int>(
+                new ClassOfEquivalenceSet(
+                    new SortedSet<ClassOfEquivalence>(
+                        EnumerateHelper.Sequence(
+                            new ClassOfEquivalence(
+                                1,
+                                new SortedSet<char>(
+                                    EnumerateHelper.Sequence('a', 'b')
+                                ).AsReadOnly()
+                            )
+                        )
+                    )
+                ),
+                new ClassOfEquivalenceSet(
+                    new SortedSet<ClassOfEquivalence>(
+                        EnumerateHelper.Sequence(
+                            new ClassOfEquivalence(
+                                1,
+                                new SortedSet<char>(
+                                    EnumerateHelper.Sequence('c', 'd')
+                                ).AsReadOnly()
+                            )
+                        )
+                    )
+                ),
+                -2
+            ),
+            new Tuple<ClassOfEquivalenceSet, ClassOfEquivalenceSet, int>(
+                new ClassOfEquivalenceSet(
+                    new SortedSet<ClassOfEquivalence>(
+                        EnumerateHelper.Sequence(
+                            new ClassOfEquivalence(
+                                1,
+                                new SortedSet<char>(
+                                    EnumerateHelper.Sequence('c', 'd')
+                                ).AsReadOnly()
+                            )
+                        )
+                    )
+                ),
+                new ClassOfEquivalenceSet(
+                    new SortedSet<ClassOfEquivalence>(
+                        EnumerateHelper.Sequence(
+                            new ClassOfEquivalence(
+                                1,
+                                new SortedSet<char>(
+                                    EnumerateHelper.Sequence('a', 'b')
+                                ).AsReadOnly()
+                            )
+                        )
+                    )
+                ),
+                2
+            ),
+            new Tuple<ClassOfEquivalenceSet, ClassOfEquivalenceSet, int>(
+                new ClassOfEquivalenceSet(
+                    new SortedSet<ClassOfEquivalence>(
+                        EnumerateHelper.Sequence(
+                            new ClassOfEquivalence(
+                                1,
+                                new SortedSet<char>(
+                                    EnumerateHelper.Sequence('a', 'b')
+                                ).AsReadOnly()
+                            )
+                        )
+                    )
+                ),
+                new ClassOfEquivalenceSet(
+                    new SortedSet<ClassOfEquivalence>(
+                        EnumerateHelper.Sequence(
+                            new ClassOfEquivalence(
+                                1,
+                                new SortedSet<char>(
+                                    EnumerateHelper.Sequence('a', 'b')
+                                ).AsReadOnly()
+                            )
+                        )
+                    )
+                ),
+                0
+            )
+        };
 
+        [Test]
+        public void CompareTest()
+        {
+            ComparableEquatableHelper.TestCompare(_Expectations);       
+        }
+
+        [Test]
+        public void EqualsTest()
+        {
+            ComparableEquatableHelper.TestEquals(_Expectations);
+        }
+
+        [Test]
+        public void GetHashCode()
+        {
+            ClassOfEquivalenceSet classOfEquivalenceSet = new ClassOfEquivalenceSet(
+                    new SortedSet<ClassOfEquivalence>(
+                        EnumerateHelper.Sequence(
+                            new ClassOfEquivalence(
+                                1,
+                                new SortedSet<char>(
+                                    EnumerateHelper.Sequence('a', 'b')
+                                ).AsReadOnly()
+                            )
+                        )
+                    )
+                );
+
+            Assert.AreEqual(196610, classOfEquivalenceSet.GetHashCode());
+        }
     }
 }
