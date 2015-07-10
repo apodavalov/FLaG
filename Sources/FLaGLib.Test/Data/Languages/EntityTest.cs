d2ea987e36833fe3cb0ce2ab888dc725d94481fb
Lenuza Podavalova <l.podavalova@gmail.com>
2015-07-11 01:32:37 +0400

    Fixed tests for Entity class

--- Sources/FLaGLib.Test/Data/Languages/EntityTest.cs
+++ Sources/FLaGLib.Test/Data/Languages/EntityTest.cs
@@ -1,12 +1,80 @@
 ﻿using FLaGLib.Data.Languages;
 using FLaGLib.Helpers;
+using FLaGLib.Test.TestHelpers;
 using NUnit.Framework;
+using System;
 
 namespace FLaGLib.Test.Data.Languages
 {
     [TestFixture]
     public class EntityTest
     {
+        private Tuple<Entity, Entity, int>[] _Expectations = new Tuple<Entity, Entity, int>[]
+        {
+            new Tuple<Entity, Entity, int>(
+                null,
+                null,
+                0
+            ),
+
+            new Tuple<Entity, Entity, int>(
+                new Degree(
+                        new Symbol('a'),
+                        new Quantity(1)
+                    ),
+                null,
+                1
+            ),
+
+            new Tuple<Entity, Entity, int>(
+                new Degree(
+                        new Symbol('a'),
+                        new Quantity(1)
+                    ),
+                                new Degree(
+                        new Symbol('a'),
+                        new Quantity(1)
+                    ),
+                0
+            ),
+
+            new Tuple<Entity, Entity, int>(
+                new Degree(
+                        new Symbol('a'),
+                        new Quantity(1)
+                    ),
+                new Degree(
+                        new Symbol('b'),
+                        new Quantity(2)
+                    ),
+                -1
+            ),
+
+            new Tuple<Entity, Entity, int>(
+                new Degree(
+                        new Symbol('a'),
+                        new Quantity(1)
+                    ),
+                new Degree(
+                        new Symbol('a'),
+                        new Quantity(1)
+                    ),
+                0
+            ),
+        };
+
+        [Test]
+        public void CompareTest()
+        {
+            ComparableEquatableHelper.TestCompare(_Expectations);
+        }
+
+        [Test]
+        public void EqualsTest()
+        {
+            ComparableEquatableHelper.TestEquals(_Expectations);
+        }
+
         [Test]
         public void SplitTest()
         {
